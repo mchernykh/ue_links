1b74a84b89c581850b3df3feb1dd7bf4ce028ed8
mchernykh <mikhail.chernykh@gmail.com>
2018-10-11 01:25:48 +0300

    .h and .cpp pages work now. guaranteed execution of script

--- convert_ue_links.js
+++ convert_ue_links.js
@@ -5,31 +5,35 @@ function onError(error) {
 function onGot(result) {
   var path = result[Object.keys(result)[0]] || "https://github.com/EpicGames/UnrealEngine/blob/release/Engine/Source/";
 
-  window.addEventListener("load", function () {
+  change_links = function () {
     var references = document.getElementById('references');
     if (references == null) {
       return;
     }
-    var descs = references.getElementsByTagName('td');
-    if (descs.length != 4) {
-      return;
+    var links = references.querySelectorAll('tr > td > p');
+    for (var i = 3; i < links.length; i = i + 2) {
+      var paragraph = links[i];
+      var address = paragraph.innerHTML;
+
+      var address_prefix = path;
+      var h_address = address_prefix + address;       
+      paragraph.innerHTML = '<a href="' + h_address + '">' + address + '</a>';
+
+      if (links.length == 4) {
+        var cpp_address = h_address.substring(0, h_address.length - 1) + 'cpp';
+        cpp_address = cpp_address.replace('/Public/', '/Private/');
+        cpp_address = cpp_address.replace('/Classes/', '/Private/');
+        paragraph.innerHTML = paragraph.innerHTML + ' <a href="' + cpp_address + '">cpp?</a>';
+      }
     }
+  };
 
-    var link_cell = descs[descs.length - 1];
-    var elem = references.querySelectorAll('tr:last-child >td:last-child > p');
-    var paragraph = elem[0];
-    var address = paragraph.innerHTML;
-
-    var address_prefix = path;
-    var h_address = address_prefix + address;       
-    paragraph.innerHTML = '<a href="' + h_address + '">' + address + '</a>';
-
-    cpp_address = h_address.substring(0, h_address.length - 1) + 'cpp';
-    cpp_address = cpp_address.replace('/Public/', '/Private/');
-    cpp_address = cpp_address.replace('/Classes/', '/Private/');
-    paragraph.innerHTML = paragraph.innerHTML + ' <a href="' + cpp_address + '">cpp?</a>';
-
-  }, false);
+  var state = document.readyState;
+  if(state === 'interactive' || state === 'complete') {
+    change_links();
+  } else {
+    document.addEventListener("DOMContentLoaded", change_links, false);
+  }
 }
 
 var getting = browser.storage.sync.get("uepath");
